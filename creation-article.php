a9a4cdcae0e5f7bc58e673f6af41768684bc2b29
joris-landaret <joris.landaret@laplateforme.io>
2023-03-09 16:03:40 +0100

    html : change name path to css

--- creation-article.php
+++ creation-article.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création d'article</title>
-    <link rel="stylesheet" href="toolbar.css">
+    <link rel="stylesheet" href="css/toolbar.css">
 </head>
 
 <body>
