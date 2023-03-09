d5e68222f34b1b290c93a0160badc25632d146df
joris-landaret <joris.landaret@laplateforme.io>
2023-03-09 14:54:58 +0100

    creation file php with basics HTML tag + div with their id and a button

---
+++ création-article.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Création d'article</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+    <main>
+        <div>
+
+            <div id="onglet"></div>
+
+            <div id="outils">
+
+            </div>
+
+        </div>
+
+        <button>Envoyer</button>
+    </main>
+
+</body>
+
+</html>
