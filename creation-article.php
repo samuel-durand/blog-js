a9622241c336ab6ccab5e5616afa8a39d2c9a32e
joris-landaret <joris.landaret@laplateforme.io>
2023-03-09 16:02:35 +0100

    html : link css

--- creation-article.php
+++ creation-article.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création d'article</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="toolbar.css">
 </head>
 
 <body>
