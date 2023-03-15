719b47100c67b98a69f2b3e20800275775b9b51f
joris-landaret <joris.landaret@laplateforme.io>
2023-03-15 16:13:31 +0100

    html : add a script tag to link it to the js file

--- creation-article.php
+++ creation-article.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création d'article</title>
     <link rel="stylesheet" href="css/toolbar.css">
+    <script async src="creation-article.js"></script>
 </head>
 
 <body>
