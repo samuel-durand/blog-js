5a1893a26534d4e036abec8ba419a138a5350bb6
joris-landaret <joris.landaret@laplateforme.io>
2023-03-15 18:00:31 +0100

    js : create a function format

--- creation-article.js
+++ creation-article.js
@@ -1,1 +1,8 @@
-console.log("test");
+// function 
+function format(f) {
+    var str = document.selection.createRange().text;
+    document.article.text1.focus();
+    var sel = document.selection.createRange();
+    sel.text = "<" + f + ">" + str + "</" + f + ">";
+    return;
+}
