d96382f8fcb47696f69758ffc7b2d2881a3ad6c0
joris-landaret <joris.landaret@laplateforme.io>
2023-03-15 18:09:10 +0100

    js : select images

--- creation-article.js
+++ creation-article.js
@@ -6,3 +6,12 @@ function format(f) {
     sel.text = "<" + f + ">" + str + "</" + f + ">";
     return;
 }
+
+// Select
+
+const bold = document.querySelector('#bold');
+const italic = document.querySelector('#italic');
+const underligne = document.querySelector('#underligne');
+const start = document.querySelector('#start');
+const center = document.querySelector('#center');
+const end = document.querySelector('#end');
