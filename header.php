37b73d5749af2d0b5dcc45ba91f2321b96e5d773
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-03-16 21:43:33 +0100

    header responsive mobile menu

--- header.php
+++ header.php
@@ -4,21 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon site</title>
     <link rel="stylesheet" href="header.css">
 </head>
 <body>
     <header>
         <nav>
+            <div class="mobile-menu-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <ul>
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="register.php">S'inscrire</a></li>
                 <li><a href="login.php">Connexion</a></li>
                 <li><a href="article.php">Article</a></li>
                 <?php if(isset($_SESSION['login']));?>
-                <li><a href="creation-article.php">crée un article</a></li>
-                <li><a href="logout.php">se déconnecter</a></li>
+                <li><a href="creation-article.php">Créer un article</a></li>
+                <li><a href="logout.php">Se déconnecter</a></li>
             </ul>
         </nav>
     </header>
     
+    <script src="menu.js">
+        const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const navList = document.querySelector('header nav ul');
+
+mobileMenuIcon.addEventListener('click', function() {
+  mobileMenuIcon.classList.toggle('active');
+  navList.classList.toggle('show');
+});
+
+    </script>
+</body>
 </html>
