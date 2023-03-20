cd1a67abbf394208a55a987cccebdd9c756f0369
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-03-20 16:36:35 +0100

    UPDATE

---
+++ article.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="comment.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include('header.php') ?>
+
+        <main>
+                <h1>TITRE</h1>
+                <textarea name="" id="" cols="30" rows="10"></textarea>        
+                
+
+        </main>
+    
+    
+
+
+
+
+
+
+    <?php include('footer.php') ?>
+    
+</body>
+</html>
