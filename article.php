cce7a78fe3f1655a0fbc7d3a0b5e3ed98a1ee6d4
joris-landaret <joris.landaret@laplateforme.io>
2023-03-22 14:36:35 +0100

    php : Connect to database

--- article.php
+++ article.php
@@ -1,5 +1,21 @@
+<?php
+
+session_start();
+
+// Connect to database
+try {
+    $database = new PDO('mysql:host=localhost;dbname=blog_js;charset=utf8;port=3307', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +23,7 @@
     <link rel="stylesheet" href="comment.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php include('header.php') ?>
 
@@ -25,10 +42,11 @@
         <label for="commentaire">Commentaire :</label>
         <textarea name="commentaire" id="commentaire"></textarea>
         <form method="post">
-        <button type="submit" name="submit">Commenter</button> 
-        </form>       
+            <button type="submit" name="submit">Commenter</button>
+        </form>
     </main>
 
     <?php include('footer.php') ?>
 </body>
+
 </html>
